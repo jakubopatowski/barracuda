33ec13209c7f4cff9beb419ce0db525fa54624bc
Jakub Opatowski <jakub.opatowski@gmail.com>
2019-06-26 22:35:29 +0200

    makefile to cmake: creation of cmake added

--- emacs/wolverine/main.py
+++ emacs/wolverine/main.py
@@ -39,21 +39,35 @@ class MakefileParser:
             # target type
             target_type = re.findall(self.target_type_pattern, makefile_data)
 
+            print('list_of_targets:')
             print(list_of_targets)
+
+            print('list_of_defines:')
             print(list_of_defines)
+
+            print('list_of_cxx_flags:')
             print(list_of_cxx_flags)
+
+            print('list_of_includes:')
             print(list_of_includes)
+
+            print('target_type:')
             print(target_type)
 
     def make_cmake(self, path_to_cmake):
         cmake = open(path_to_cmake, 'w')
-        cmake.write()
+        cmake.write('cmake_minimum_required(VERSION 3.1...3.15)\n')
+        cmake.write('\n')
+        cmake.write('if(${CMAKE_VERSION} VERSION_LESS 3.12)\n')
+        cmake.write('    cmake_policy(VERSION ${CMAKE_MAJOR_VERSION}')
+        cmake.write('.${CMAKE_MINOR_VERSION})\n')
+        cmake.write('endif()\n')
+        cmake.write('\\n')
 
 
 project = 'radosc'
-makefile_path = os.path.join('c:\\', 'Projekty', 'trunk',
-                             'win32-msvc2015_d', project,
-                             'qt4', 'makefile')
+makefile_path = os.path.join('makefile')
 
 makefile_parser = MakefileParser(makefile_path)
 makefile_parser.parse_file()
+makefile_parser.make_cmake('CMakeLists.txt')
