a31ec5b5ca0fe1defbca568a0cf81a7b3361305e
Jakub Opatowski <jakub.opatowski@gmail.com>
2019-06-27 15:45:15 +0200

    Finished basic makefile to cmake converter; added font ligatures to emacs

--- emacs/wolverine/main.py
+++ emacs/wolverine/main.py
@@ -4,6 +4,8 @@ import os
 
 class MakefileParser:
 
+    line_continue_pattern = r'\\\n'
+    # co robi r ?
     target_pattern = 'TARGET\\s*=\\s*(\\w*)'
     defines_pattern = 'DEFINES\\s*=\\s*(.*)'
     define_pattern = '-D(\\S*)'
@@ -12,6 +14,8 @@ class MakefileParser:
     includes_pattern = 'INCPATH\\s*=\\s(.*)'
     include_pattern = '\\"(\\S*)\\"'
     target_type_pattern = '\\#\\s*Template:\\s*([\\w]*)'
+    sources_pattern = 'SOURCES\\s*=\\s*(.*)'
+    # source_pattern =
 
     def __init__(self, makefile_path):
         self.makefile_path = makefile_path
@@ -21,38 +25,46 @@ class MakefileParser:
             with open(self.makefile_path) as makefile:
                 makefile_data = makefile.read()
 
+            # delete line continuations
+            makefile_data = re.sub(self.line_continue_pattern, '',
+                                   makefile_data)
+
+            new_makefile = open('new_makefile', 'w')
+            new_makefile.write(makefile_data)
+            new_makefile.close()
+
             # targets
-            list_of_targets = re.findall(self.target_pattern, makefile_data)
+            self.list_of_targets = re.findall(self.target_pattern,
+                                              makefile_data)
+            print('list_of_targets:', self.list_of_targets)
 
             # defines
             defines = re.findall(self.defines_pattern, makefile_data)
-            list_of_defines = re.findall(self.define_pattern, defines[0])
+            self.list_of_defines = re.findall(self.define_pattern,
+                                              defines[0])
+            print('list_of_defines:', self.list_of_defines)
 
             # cxx flags
             cxx_flags = re.findall(self.cxx_flags_pattern, makefile_data)
-            list_of_cxx_flags = re.findall(self.cxx_flag_pattern, cxx_flags[0])
+            self.list_of_cxx_flags = re.findall(self.cxx_flag_pattern,
+                                                cxx_flags[0])
+            print('list_of_cxx_flags:', self.list_of_cxx_flags)
 
             # includes
             includes = re.findall(self.includes_pattern, makefile_data)
-            list_of_includes = re.findall(self.include_pattern, includes[0])
+            self.list_of_includes = re.findall(self.include_pattern,
+                                               includes[0])
+            print('list_of_includes:', self.list_of_includes)
 
             # target type
-            target_type = re.findall(self.target_type_pattern, makefile_data)
+            self.target_type = re.findall(self.target_type_pattern,
+                                          makefile_data)
+            print('target_type:', self.target_type)
 
-            print('list_of_targets:')
-            print(list_of_targets)
-
-            print('list_of_defines:')
-            print(list_of_defines)
-
-            print('list_of_cxx_flags:')
-            print(list_of_cxx_flags)
-
-            print('list_of_includes:')
-            print(list_of_includes)
-
-            print('target_type:')
-            print(target_type)
+            # sources
+            sources = re.findall(self.sources_pattern, makefile_data)
+            self.list_of_sources = sources[0].split()
+            print('sources:', self.list_of_sources)
 
     def make_cmake(self, path_to_cmake):
         cmake = open(path_to_cmake, 'w')
@@ -62,7 +74,42 @@ class MakefileParser:
         cmake.write('    cmake_policy(VERSION ${CMAKE_MAJOR_VERSION}')
         cmake.write('.${CMAKE_MINOR_VERSION})\n')
         cmake.write('endif()\n')
-        cmake.write('\\n')
+        cmake.write('\n')
+        cmake.write('if(MSVC)\n')
+        cmake.write('    message(status \"Setting MSVC flags\")\n')
+        cmake.write('    set(CMAKE_CXX_FLAGS\n')
+        for cxx_flag in self.list_of_cxx_flags:
+            cmake.write('        ')
+            cmake.write(cxx_flag)
+            cmake.write('\n')
+        for define in self.list_of_defines:
+            cmake.write('        -D')
+            cmake.write(define)
+            cmake.write('\n')
+
+        cmake.write('    )\n')
+        cmake.write('endif()\n')
+        cmake.write('set(CMAKE_EXPORT_COMPILE_COMMANDS ON)\n')
+        cmake.write('message(status "** CMAKE_CXX_FLAGS: ')
+        cmake.write('${CMAKE_CXX_FLAGS}")\n')
+        cmake.write('set(project_sources\n')
+        for source in self.list_of_sources:
+            cmake.write('    ')
+            cmake.write(source)
+            cmake.write('\n')
+
+        cmake.write(')\n')
+        if self.target_type[0] == 'lib':
+            cmake.write('add_library(${PROJECT_NAME}\n')
+            cmake.write('    ${project_sources})\n')
+
+        cmake.write('target_include_directories(${PROJECT_NAME}\n')
+        for include in self.list_of_includes:
+            cmake.write('    ')
+            cmake.write(include)
+            cmake.write('\n')
+
+        cmake.write(')\n')
 
 
 project = 'radosc'
